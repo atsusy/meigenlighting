4cabf4ef8e37acf30c7b8e772a1d8740dcb0b491
KATAOKA,Atsushi <atsusy@i.softbank.jp>
2011-04-18 11:40:29 +0900

    initial commit.

---
+++ js/iphone.js
@@ -0,0 +1,4 @@
+// アドレスバーを非表示にする
+function doScroll() { if (window.pageYOffset === 0) { window.scrollTo(0,1); } }
+window.addEventListener('load', function () { setTimeout(doScroll, 100); }, false);
+window.onorientationchange = function() { setTimeout(doScroll, 100); };
